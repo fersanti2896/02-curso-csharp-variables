61047f8def7f06d985b8d4c7aeb69b1f2ea3c8af
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-23 17:01:53 -0500

    Tipo de Referencias No Nulos

---
+++ Codigo/10-tipos-referencia-no-nulos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _10_tipos_referencia_no_nulos {
+        public void tipoReferenciaNoNulos() {
+            int? edad = null;
+            Console.WriteLine("Tipo de Referecia: " + edad);
+
+            string? nombre = "Fernando Nicolás";
+            Console.WriteLine("Valor por Defecto String: " + nombre.ToUpper());
+        }
+    }
+}
