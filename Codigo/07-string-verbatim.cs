6939c26431840ab03e8ddb2aa0c8f1e181b6ec10
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-23 13:28:48 -0500

    String verbatim en 07-string-verbatim.cs

---
+++ Codigo/07-string-verbatim.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _07_string_verbatim {
+        public void stringVerbatim() {
+            string carta = @"A quien pueda interesar:
+            Estimado cliente, le invitamos hacer su pago lo más pronto posible.
+
+            Atte. Empresa
+            ";
+            Console.WriteLine(carta);
+
+            string directorio = @"Directorio: C:\usuario\fersa";
+            Console.WriteLine(directorio);
+
+        }
+    }
+}
