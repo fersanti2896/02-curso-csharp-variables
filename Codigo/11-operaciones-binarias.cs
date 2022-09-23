532a2a9a3bba8cc06d6a57e5cea7947884adebcb
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-23 17:28:58 -0500

    Operaciones binarias en 11-operaciones-binarias.cs

---
+++ Codigo/11-operaciones-binarias.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _11_operaciones_binarias {
+        public void operacionesBinarias() {
+            var cantidad = 1996;
+            var cantidad2 = 2000;
+            var resultado = cantidad + cantidad2;
+
+            Console.WriteLine("La suma de " + cantidad + " + " + cantidad2 + " es " + resultado);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,4 +2,3 @@
 
 Console.WriteLine();
 
-
