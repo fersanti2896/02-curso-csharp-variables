9b3a96c7f05855a14be57741afe2709029854364
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-23 13:19:13 -0500

    Char y String en 06-char-string.cs

---
+++ Codigo/06-char-string.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _06_char_string {
+        public void charString() {
+            char letra = 'A';
+            Console.WriteLine("Letra: " + letra);
+
+            string nombre = "Fer Santi";
+            Console.WriteLine("Mi nombre es: " + nombre);
+
+            string texto = "Soy un programador jr";
+            Console.WriteLine(texto);
+        }
+    }
+}
