d3335cf52f57d955235d332f406e8e3fe486197a
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-23 13:06:02 -0500

    Booleanos en 05-booleanos.cs

---
+++ Codigo/05-booleanos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _05_booleanos {
+        public void tipoBooleano() {
+            bool soyFernando = true;
+            Console.WriteLine("¿Soy Fernando?: " + soyFernando);
+
+            bool soyMayor = false;
+            Console.WriteLine("Tengo 17 años, y ¿me considero mayor?: " + soyMayor);
+        }  
+    }
+}
