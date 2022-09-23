ab999be1721761282022c14c085c6c62ce436a1f
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-23 14:31:34 -0500

    Valor por defecto en tipos en 09-valores-defecto.cs

---
+++ Codigo/09-valores-defecto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _09_valores_defecto {
+        public void valoresXDefecto() {
+
+            string apellido = default;
+            Console.WriteLine("Valor por Defecto de un String: " + apellido);
+
+            bool valor = default;
+            Console.WriteLine("Valor por Defecto de un Bool: " + valor);
+
+            DateTime fecha = default;
+            Console.WriteLine("Valor por Defecto de una Fecha: " + fecha);
+
+            decimal numero = default;
+            Console.WriteLine("Valor por Defecto de una Decimal: " + numero);
+        }
+    }
+}
