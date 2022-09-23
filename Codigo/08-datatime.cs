ee0bc9b61862e3bd13080706754686fa430d3055
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-23 13:51:20 -0500

    Fechas en 08-datetime.cs

---
+++ Codigo/08-datatime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _08_datatime {
+        public void datatime() {
+            DateTime fechaAct = DateTime.Now;
+            Console.WriteLine("Fecha Actual: " + fechaAct.DayOfWeek + " " + fechaAct);
+
+            DateTime fechaNac = new DateTime(1996, 12, 16);
+            Console.WriteLine("Fecha de Nacimiento: " + fechaNac);
+
+            DateTime fechaMan = DateTime.Now.AddDays(1);
+            Console.WriteLine("Mañana es: " + fechaMan.DayOfWeek + " " + fechaMan);
+        }
+    }
+}
