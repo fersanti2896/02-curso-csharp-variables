5d98d203cfdb40f0380b22a441906b0d4048afa7
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-26 10:43:15 -0500

    Operaciones de Asignacion

---
+++ Codigo/14-operaciones-asignacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _14_operaciones_asignacion {
+        public void operacionesAsignacion() {
+            var num = 16;
+            var num2 = 25;
+
+            Console.WriteLine($"Valor 1 Inicial: { num }");
+            Console.WriteLine($"Valor 2 Inicial: { num2 }");
+
+            num += num2;
+            Console.WriteLine($"Valor 1 Actual: { num }");
+
+            num2 += num;
+            Console.WriteLine($"Valor 2 Actual: { num2 }");
+        }
+    }
+}
