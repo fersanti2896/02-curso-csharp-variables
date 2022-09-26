18919f7376eba20cedec58215edd100271eaec12
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-26 09:57:06 -0500

    Operaciones binarias en 11-operaciones-binarias.cs

--- Codigo/11-operaciones-binarias.cs
+++ Codigo/11-operaciones-binarias.cs
@@ -7,11 +7,34 @@ using System.Threading.Tasks;
 namespace Variables.Codigo {
     public class _11_operaciones_binarias {
         public void operacionesBinarias() {
-            var cantidad = 1996;
-            var cantidad2 = 2000;
-            var resultado = cantidad + cantidad2;
+            var num = 1996;
+            var num2 = 2000;
+            var resultado = num + num2;
 
-            Console.WriteLine("La suma de " + cantidad + " + " + cantidad2 + " es " + resultado);
+            Console.WriteLine("La suma de " + num + " + " + num + " es " + resultado);
+
+            var cantidad = 7.1m;
+            var cantidad2 = 3.5m;
+            var suma = cantidad + cantidad2;
+            var resta = cantidad - cantidad2;
+            var multiplicacion = cantidad * cantidad2;
+            var division = cantidad / cantidad2;
+            var residuo = cantidad % cantidad2;
+
+            var msgSum = "La suma es: " + suma;
+            Console.WriteLine(msgSum);
+
+            var msgRes = "La resta es: " + resta;
+            Console.WriteLine(msgRes);
+
+            var msgMul = "La multiplicacion es: " + multiplicacion;
+            Console.WriteLine(msgMul);
+
+            var msgDiv = "La division es: " + division;
+            Console.WriteLine(msgDiv);
+
+            var msgResiduo = "El residuo es: " + residuo;
+            Console.WriteLine(msgResiduo);
         }
     }
 }
