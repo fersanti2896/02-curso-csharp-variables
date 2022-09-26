046f7a9afc589a56a211f14b8b388a42adbb0a8f
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-26 10:03:01 -0500

    Interpolacion de String en 12-interpolacion-strings.cs

---
+++ Codigo/12-interpolacion-strings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _12_interpolacion_strings {
+        public void interpolacionString() {
+            var num = 32;
+            var num2 = 64;
+
+            Console.WriteLine($"Hay dos sistemas de bits { num } y { num2 } para SO!");
+
+            var nombre = "Fernando";
+            var apellido = "Nicolás";
+
+            Console.WriteLine($"Mi nombre es { nombre } { apellido }");
+        }
+    }
+}
