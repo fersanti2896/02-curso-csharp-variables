b29b1405b29cff35b683dd307fd3019ca4241acb
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-26 10:26:15 -0500

    Operaciones unarias

---
+++ Codigo/13-operaciones-unarias.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _13_operaciones_unarias {
+        public void operacionesUnarias() {
+            var num = 19;
+            Console.WriteLine(-num);
+
+            num++;
+            Console.WriteLine($"Suma del valor: { num } ");
+
+            num--;
+            Console.WriteLine($"Resta del valor: { num } ");
+
+            ++num;
+            Console.WriteLine($"Suma del valor 2: { num }");
+
+            --num;
+            Console.WriteLine($"Resta del valor2: { num } ");
+        }
+    }
+}
