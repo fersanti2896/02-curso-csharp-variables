1690ced996909502ea7d6e3a069b95d5d4f73fa9
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-26 10:55:37 -0500

    Valores constantes

---
+++ Codigo/15-constantes-valoresMag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Codigo {
+    public class _15_constantes_valoresMag {
+        public void constantesValoresMagios() {
+            const decimal valor = 3.1416m;
+            Console.WriteLine($"Valor de PI: { valor }");
+
+            const double factor = 2.1;
+            Console.WriteLine($"Factor constante: { factor }");
+        }
+    }
+}
